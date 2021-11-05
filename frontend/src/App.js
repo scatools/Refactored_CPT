d6ca056302590879c4ee24f4f1122845fb4f7cb4
lymine1996 <lymine1996@163.com>
2021-11-05 16:00:18 -0500

    Create single AOI reports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import NavBar from "./NavBar";
 import Routes from "./Routes";
@@ -7,6 +7,7 @@ import BarLoader from "react-spinners/BarLoader";
 import { connect } from "react-redux";
 
 function App(props) {
+  const [ reportLink, setReportLink ] = useState(false);
   return (
     <LoadingOverlay
       className="myLoading"
@@ -15,9 +16,9 @@ function App(props) {
       text="Loading..."
     >
       <div className="App">
-        <NavBar />
+        <NavBar reportLink={reportLink}/>
         <div style={{ position: "relative", top: "55px" }}>
-          <Routes />
+          <Routes setReportLink={setReportLink}/>
         </div>
       </div>
     </LoadingOverlay>
