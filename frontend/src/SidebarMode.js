af14ce76710e0d320d71d3ed0544ac99a0dd222e
Anthony Collini <collini.anthony@gmail.com>
2021-11-16 06:13:03 -0600

    Clean up code formatting. Change state name from mode/setMode to view/setView as the variable mode is used in multiple places

--- frontend/src/SidebarMode.js
+++ frontend/src/SidebarMode.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { ButtonGroup, ToggleButton } from 'react-bootstrap';
+import React from "react";
+import { ButtonGroup, ToggleButton } from "react-bootstrap";
 
-const SidebarMode = ({ mode, setMode }) => {
-	return (
-		<ButtonGroup toggle className="d-flex justify-content-center">
-			<ToggleButton
-				type="radio"
-				variant="outline-secondary"
-				name="add"
-				value="add"
-				checked={mode === 'add'}
-				onChange={(e) => setMode(e.currentTarget.value)}
-			>
-				Add New
-			</ToggleButton>
-			<ToggleButton
-				type="radio"
-				variant="outline-secondary"
-				name="view"
-				value="view"
-				checked={mode === 'view'}
-				onChange={(e) => setMode(e.currentTarget.value)}
-			>
-				View Current
-			</ToggleButton>
-			<ToggleButton
-				type="radio"
-				variant="outline-secondary"
-				name="assemble"
-				value="assemble"
-				checked={mode === 'assemble'}
-				onChange={(e) => setMode(e.currentTarget.value)}
-			>
-				Create Assessment
-			</ToggleButton>
-		</ButtonGroup>
-	);
+const SidebarMode = ({ view, setView }) => {
+  return (
+    <ButtonGroup toggle className="d-flex justify-content-center">
+      <ToggleButton
+        type="radio"
+        variant="outline-secondary"
+        name="add"
+        value="add"
+        checked={view === "add"}
+        onChange={(e) => setView(e.currentTarget.value)}
+      >
+        Add New
+      </ToggleButton>
+      <ToggleButton
+        type="radio"
+        variant="outline-secondary"
+        name="viewCurrent"
+        value="viewCurrent"
+        checked={view === "viewCurrent"}
+        onChange={(e) => setView(e.currentTarget.value)}
+      >
+        View Current
+      </ToggleButton>
+      <ToggleButton
+        type="radio"
+        variant="outline-secondary"
+        name="assemble"
+        value="assemble"
+        checked={view === "assemble"}
+        onChange={(e) => setView(e.currentTarget.value)}
+      >
+        Create Assessment
+      </ToggleButton>
+    </ButtonGroup>
+  );
 };
 
 export default SidebarMode;
