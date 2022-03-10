209d14fbee4f0fb5195145d10229905f99ef2453
Anthony Collini <collini.anthony@gmail.com>
2022-03-10 12:13:18 -0600

    aAdd bug icon/link for bug reporting

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -5,11 +5,14 @@ import Map from "./Map";
 import AoiDetailTable from "./ViewCurrentAOI/AoiDetailTable";
 import { DrawPolygonMode, EditingMode } from "react-map-gl-draw";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faBug } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 
 const arrowIcon = (
   <FontAwesomeIcon icon={faArrowRight} color="white" size="lg" />
 );
+
+const bugIcon = <FontAwesomeIcon icon={faBug} color="red" size="2x" />;
 
 const Main = ({
   aoiSelected,
@@ -42,6 +45,8 @@ const Main = ({
   const [hexIDDeselected, setHexIDDeselected] = useState([]);
   const [hexFilterList, setHexFilterList] = useState([]);
 
+  const history = useHistory();
+
   const autoDraw = async () => {
     setMode(new DrawPolygonMode());
     // Use crosshair as cursor style when drawing new shapes over SCA boundary
@@ -58,6 +63,9 @@ const Main = ({
 
   return (
     <div>
+      <Button onClick={() => history.push("/help")} className="bug-icon-btn">
+        {bugIcon}
+      </Button>
       <AoiDetailTable
         activeTable={activeTable}
         setActiveTable={setActiveTable}
