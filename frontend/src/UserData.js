4c1e2e101973420e28eeb6c916125beaeab3b477
lymine1996 <lymine1996@163.com>
2022-03-14 10:00:50 -0500

    Allow users to update their profile information

--- frontend/src/UserData.js
+++ frontend/src/UserData.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Container, Jumbotron } from "react-bootstrap";
+import { Button, Container, Jumbotron, Modal } from "react-bootstrap";
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from "react-redux";
 import { v4 as uuid } from "uuid";
@@ -14,14 +14,25 @@ const UserData = ({ userLoggedIn, setReportScript, setAlertText, setAlertType })
   const [ username, setUsername ] = useState(null);
   const [ password, setPassword ] = useState(null);
   const [ firstName, setFirstName ] = useState(null);
+  const [ newFirstName, setNewFirstName ] = useState(null);
   const [ lastName, setLastName ] = useState(null);
+  const [ newLastName, setNewLastName ] = useState(null);
   const [ email, setEmail ] = useState(null);
+  const [ newEmail, setNewEmail ] = useState(null);
   const [ admin, setAdmin ] = useState(false);
   const [ userFileList, setUserFileList ] = useState([]);
   const [ userReportList, setUserReportList ] = useState([]);
   const [ fileDeleted, setFileDeleted ] = useState(null);
   const [ reportDeleted, setReportDeleted ] = useState(null);
-  
+  const [ updateInfo, setUpdateInfo ] = useState(false);
+
+  const showUpdateInfo = () => setUpdateInfo(true);
+
+  const closeUpdateInfo = () => {
+    setUpdateInfo(false);
+    getUserData();
+  };
+
   const getUserData = async () => {
     // For development on local server
     // const result = await axios.post(
@@ -38,9 +49,41 @@ const UserData = ({ userLoggedIn, setReportScript, setAlertText, setAlertType })
     setUsername(result.data.rows[0].username);
     setPassword(result.data.rows[0].password);
     setFirstName(result.data.rows[0].first_name);
+    setNewFirstName(result.data.rows[0].first_name);
     setLastName(result.data.rows[0].last_name);
+    setNewLastName(result.data.rows[0].last_name);
     setEmail(result.data.rows[0].email);
+    setNewEmail(result.data.rows[0].email);
     setAdmin(result.data.rows[0].is_admin);
+  };
+
+  const updateUserInfo = async () => {
+    // For development on local server
+    // const result = await axios.post(
+    //   'http://localhost:5000/update/information',
+    //   {
+    //     username: userLoggedIn,
+    //     email: newEmail,
+    //     first_name: newFirstName,
+    //     last_name: newLastName
+    //   }
+    // );
+    
+    // For production on Heroku
+    const result = await axios.post(
+      'https://sca-cpt-backend.herokuapp.com/update/information',
+      {
+        username: userLoggedIn,
+        email: newEmail,
+        first_name: newFirstName,
+        last_name: newLastName
+      }
+    );
+    if (result) {
+      setAlertType("success");
+      setAlertText("You have updated your profile!");
+      closeUpdateInfo();
+    };
   };
 
   const getUserFile = async () => {
@@ -210,19 +253,19 @@ const UserData = ({ userLoggedIn, setReportScript, setAlertText, setAlertType })
           <p>Your email: {email}</p>
           <div className="d-flex justify-content-between btn-container">
             {admin && (
-              <a className="btn btn-success" href="/admin">
-                Admin module
-              </a>
+              <Button className="btn btn-success">
+                Admin Module
+              </Button>
             )}
-            <a className="btn btn-success" href="/users/profile">
-              Update user information
-            </a>
-            <a className="btn btn-success" href="/users/changepassword">
-              Change password
-            </a>
-            <a className="btn btn-danger" href="/">
-              Delete user
-            </a>
+            <Button className="btn btn-success" onClick={showUpdateInfo}>
+              Update Information
+            </Button>
+            <Button className="btn btn-success">
+              Change Password
+            </Button>
+            <Button className="btn btn-danger">
+              Delete Account
+            </Button>
           </div>
   
           <hr className="my-4" />
@@ -281,6 +324,51 @@ const UserData = ({ userLoggedIn, setReportScript, setAlertText, setAlertType })
             <p className="lead">No report saved yet!</p>
           )}
         </Jumbotron>
+        <Modal centered show={updateInfo} onHide={closeUpdateInfo} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Please enter your profile information here
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <div className="form-group">
+                Your Username: 
+                <input type="text" value={username} disabled></input>
+                Your Email: 
+                <input
+                  type="text"
+                  value={newEmail}
+                  onChange={(e)=>setNewEmail(e.target.value)}
+                  required>
+                </input>
+                Your First Name: 
+                <input
+                  type="text"
+                  value={newFirstName}
+                  onChange={(e)=>setNewFirstName(e.target.value)}
+                  required>
+                </input>
+                Your Last Name: 
+                <input
+                  type="text"
+                  value={newLastName}
+                  onChange={(e)=>setNewLastName(e.target.value)}
+                  required>
+                </input>
+                <br/>
+                <div className="d-flex justify-content-between">
+                  <Button className="btn btn-warning" onClick={closeUpdateInfo}>
+                    Cancel
+                  </Button>
+                  <Button className="btn btn-primary" onClick={updateUserInfo}>
+                    Confirm
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </Modal.Body>
+        </Modal>
       </Container>
     );
   }
