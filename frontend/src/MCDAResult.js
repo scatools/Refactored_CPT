92f0340d69b24c5667985dfd3566bc25565032de
Anthony Collini <collini.anthony@gmail.com>
2022-05-02 15:33:30 -0500

    Add loading spinner to initial screen while react is loading.

--- frontend/src/MCDAResult.js
+++ frontend/src/MCDAResult.js
@@ -1,142 +1,222 @@
-import React, { useState, useEffect } from 'react';
-import { ButtonGroup, ToggleButton, Row, Col } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { v4 as uuid } from 'uuid';
-import { Cell, PieChart, Pie, Sector, Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from "recharts";
+import React, { useState, useEffect } from "react";
+import { ButtonGroup, ToggleButton, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { v4 as uuid } from "uuid";
+import {
+  Cell,
+  PieChart,
+  Pie,
+  Sector,
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+} from "recharts";
 
 const MCDAResult = () => {
-    const aoiColors = ["#00188f", "#00bcf2", "#00b294", "#009e49", "#bad80a", "#fff100", "#ff8c00", "#e81123", "#ec008c", "#68217a"];
-    const colorOpacity = ["FF", "E6", "CC", "B3", "99", "80", "66", "4D", "33", "1A"];
-    const assessment = useSelector((state) => state.assessment);
-    const [ aoi, setAoi ] = useState(0);
-    const [ fillColor, setFillColor ] = useState("#8884d8");
-    const [ pieColors, setPieColors ] = useState(aoiColors);
-    const [ activeIndex, setActiveIndex ] = useState(0);
-    const [ pieData, setPieData ] = useState([]);
-    const [ radarData, setRadarData ] = useState(
-        [
-            { subject: 'Habitat', score: 0,  fullMark: 1 },
-            { subject: 'WQ',score: 0,  fullMark: 1 },
-            { subject: 'LCMR', score: 0,  fullMark: 1 },
-            { subject: 'Community Resilience', score: 0,  fullMark: 1 },
-            { subject: 'Gulf Economy', score: 0, fullMark: 1 }
-        ]
+  const aoiColors = [
+    "#00188f",
+    "#00bcf2",
+    "#00b294",
+    "#009e49",
+    "#bad80a",
+    "#fff100",
+    "#ff8c00",
+    "#e81123",
+    "#ec008c",
+    "#68217a",
+  ];
+  const colorOpacity = [
+    "FF",
+    "E6",
+    "CC",
+    "B3",
+    "99",
+    "80",
+    "66",
+    "4D",
+    "33",
+    "1A",
+  ];
+  const assessment = useSelector((state) => state.assessment);
+  const [aoi, setAoi] = useState(0);
+  const [fillColor, setFillColor] = useState("#8884d8");
+  const [pieColors, setPieColors] = useState(aoiColors);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [pieData, setPieData] = useState([]);
+  const [radarData, setRadarData] = useState([
+    { subject: "Habitat", score: 0, fullMark: 1 },
+    { subject: "WQ", score: 0, fullMark: 1 },
+    { subject: "LCMR", score: 0, fullMark: 1 },
+    { subject: "Community Resilience", score: 0, fullMark: 1 },
+    { subject: "Gulf Economy", score: 0, fullMark: 1 },
+  ]);
+
+  useEffect(() => {
+    setRadarData((data) =>
+      [...data].map((goal, index) => {
+        return {
+          ...goal,
+          score: assessment.centralWeight[aoi][index],
+        };
+      })
     );
+    setPieData(
+      assessment.rankAccept.map((item, index) => {
+        return { name: `Rank ${index + 1}`, value: item[aoi] };
+      })
+    );
+    // Use the same AOI-based color scheme for pie chart and radar chart
+    setFillColor(aoiColors[aoi]);
+    setPieColors(
+      assessment.aoi.id.map((id, index) => {
+        return aoiColors[aoi] + colorOpacity[index];
+      })
+    );
+  }, [aoi, assessment]);
 
-    useEffect(()=>{
-        setRadarData(data=>[...data].map((goal,index)=>{
-            return {
-                ...goal,
-                score:assessment.centralWeight[aoi][index]
-            };
-        }));
-        setPieData(assessment.rankAccept.map((item,index)=>{return {name:`Rank ${index+1}`, value: item[aoi]}}));
-        // Use the same AOI-based color scheme for pie chart and radar chart
-        setFillColor(aoiColors[aoi]);
-        setPieColors(assessment.aoi.id.map((id,index)=>{return aoiColors[aoi]+colorOpacity[index]}));
-    },[aoi,assessment]);
-    
-    function onPieEnter(data, index) {
-        setActiveIndex(index);
-    };
+  function onPieEnter(data, index) {
+    setActiveIndex(index);
+  }
 
-    const renderActiveShape = (props) => {
-        const RADIAN = Math.PI / 180;
-        const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
-          fill,  percent,name } = props;
-        const sin = Math.sin(-RADIAN * midAngle);
-        const cos = Math.cos(-RADIAN * midAngle);
-        const sx = cx + (outerRadius + 10) * cos;
-        const sy = cy + (outerRadius + 10) * sin;
-        const mx = cx + (outerRadius + 30) * cos;
-        const my = cy + (outerRadius + 30) * sin;
-        const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-        const ey = my;
-        const textAnchor = cos >= 0 ? 'start' : 'end';
-      
-        return (
-          <g>
-            <Sector
-              cx={cx}
-              cy={cy}
-              innerRadius={innerRadius}
-              outerRadius={outerRadius}
-              startAngle={startAngle}
-              endAngle={endAngle}
-              fill={fill}
-            />
-            <Sector
-              cx={cx}
-              cy={cy}
-              startAngle={startAngle}
-              endAngle={endAngle}
-              innerRadius={outerRadius + 6}
-              outerRadius={outerRadius + 10}
-              fill={fill}
-            />
-            <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
-            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${name}`}</text>
-            <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-              {`(Rate ${(percent * 100).toFixed(2)}%)`}
-            </text>
-          </g>
-        );
-    };
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const {
+      cx,
+      cy,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      percent,
+      name,
+    } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
 
-	return (
-        <>
-        <Row>
-		<ButtonGroup toggle className="ml-4">
-			{assessment.aoi.id.map((item, index) => (
-				<ToggleButton
-          key={uuid()}
-					type="radio"
-					variant="outline-secondary"
-					name={index}
-					value={index}
-					checked={aoi === index}
-					onChange={(e) => setAoi(e.currentTarget.value)}
-				>
-					{assessment.aoi.name[index]}
-				</ToggleButton>
-			))}
-		</ButtonGroup>
-        <hr/>
+    return (
+      <g>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path
+          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+          stroke={fill}
+          fill="none"
+        />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          textAnchor={textAnchor}
+          fill="#333"
+        >{`${name}`}</text>
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          dy={18}
+          textAnchor={textAnchor}
+          fill="#999"
+        >
+          {`(Rate ${(percent * 100).toFixed(2)}%)`}
+        </text>
+      </g>
+    );
+  };
+
+  return (
+    <>
+      <Row>
+        <ButtonGroup toggle className="ml-4">
+          {assessment.aoi.id.map((item, index) => (
+            <ToggleButton
+              key={uuid()}
+              type="radio"
+              variant="outline-secondary"
+              name={index}
+              value={index}
+              checked={aoi === index}
+              onChange={(e) => setAoi(e.currentTarget.value)}
+            >
+              {assessment.aoi.name[index]}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
+        <hr />
         <p>Currently Viewing: {assessment.aoi.name[aoi]}</p>
-        </Row>
-        <Row>
-            <Col style={{padding:'10px'}}>
-              <PieChart width={550} height={400}>
-                <Pie 
-                  activeIndex={activeIndex}
-                  activeShape={renderActiveShape} 
-                  data={pieData} 
-                  cx={300} 
-                  cy={200} 
-                  innerRadius={60}
-                  outerRadius={80} 
-                  fill={fillColor}
-                  onMouseEnter={onPieEnter}
-                >
-                  {pieData.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={pieColors[index % 20]} opacity={0.5} />
-                  ))}
-                </Pie>
-              </PieChart>
-            </Col>
-            <Col>
-              <RadarChart cx={300} cy={250} outerRadius={150} width={500} height={500} data={radarData}>
-                <PolarGrid />
-                <PolarAngleAxis dataKey="subject" />
-                <PolarRadiusAxis/>
-                <Radar dataKey="score" stroke={fillColor} fill={fillColor} fillOpacity={0.5}/>
-              </RadarChart>
-            </Col>
-        </Row>
-        
-        </>
-	);
+      </Row>
+      <Row>
+        <Col style={{ padding: "10px" }}>
+          <PieChart width={550} height={400}>
+            <Pie
+              activeIndex={activeIndex}
+              activeShape={renderActiveShape}
+              data={pieData}
+              cx={300}
+              cy={200}
+              innerRadius={60}
+              outerRadius={80}
+              fill={fillColor}
+              onMouseEnter={onPieEnter}
+            >
+              {pieData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={pieColors[index % 20]}
+                  opacity={0.5}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </Col>
+        <Col>
+          <RadarChart
+            cx={300}
+            cy={250}
+            outerRadius={150}
+            width={500}
+            height={500}
+            data={radarData}
+          >
+            <PolarGrid />
+            <PolarAngleAxis dataKey="subject" />
+            <PolarRadiusAxis />
+            <Radar
+              dataKey="score"
+              stroke={fillColor}
+              fill={fillColor}
+              fillOpacity={0.5}
+            />
+          </RadarChart>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default MCDAResult;
--- frontend/src/ViewCurrentAOI/SidebarViewDetail.js
+++ frontend/src/ViewCurrentAOI/SidebarViewDetail.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Card, Container, Button, InputGroup, FormControl, Modal, Accordion } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  Button,
+  InputGroup,
+  FormControl,
+  Modal,
+  Accordion,
+} from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { MdViewList, MdEdit, MdDelete, MdSave } from "react-icons/md";
 import { HiDocumentReport } from "react-icons/hi";
@@ -42,7 +50,7 @@ const SidebarViewDetail = ({
   editMode,
   stopDraw,
   setAlertText,
-  setAlertType
+  setAlertType,
 }) => {
   const aoiList = Object.values(useSelector((state) => state.aoi)).filter(
     (aoi) => aoi.id === aoiSelected
@@ -57,12 +65,14 @@ const SidebarViewDetail = ({
   const [showButtonLabel, setShowButtonLabel] = useState("Show Hexagon Grid");
   const [showButtonDisabled, setShowButtonDisabled] = useState(false);
   const [deselectButtonState, setDeselectButtonState] = useState("deselect");
-  const [deselectButtonLabel, setDeselectButtonLabel] = useState("Deselect Hexagon");
+  const [deselectButtonLabel, setDeselectButtonLabel] =
+    useState("Deselect Hexagon");
   const [deselectButtonDisabled, setDeselectButtonDisabled] = useState(true);
   const [confirmButtonDisabled, setConfirmButtonDisabled] = useState(true);
   const [confirmShow, setConfirmShow] = useState(false);
 
   const modifyShape = () => {
+    console.log(aoiList[0]);
     if (modifyButtonState === "modify") {
       setModifyButtonState("finalize");
       setModifyButtonLabel("Finalize Shape");
@@ -75,7 +85,7 @@ const SidebarViewDetail = ({
       setModifyButtonLabel("Modify Shape");
       setConfirmButtonDisabled(false);
       stopDraw();
-    };
+    }
   };
 
   const handleNameEdit = async () => {
@@ -99,9 +109,9 @@ const SidebarViewDetail = ({
         })
       );
       dispatch(setLoader(false));
-    };
+    }
   };
-  
+
   const handleBasicEdit = async () => {
     if (!aoiName) {
       setAlertType("danger");
@@ -157,7 +167,7 @@ const SidebarViewDetail = ({
       }
       setDrawingMode(false);
       dispatch(setLoader(false));
-    };
+    }
   };
 
   const handleAdvancedEdit = async () => {
@@ -231,7 +241,7 @@ const SidebarViewDetail = ({
         })
       );
       dispatch(setLoader(false));
-    };
+    }
   };
 
   const showHexagon = () => {
@@ -249,7 +259,7 @@ const SidebarViewDetail = ({
       setShowButtonState("show");
       setShowButtonLabel("Show Hexagon Grid");
       setDeselectButtonDisabled(true);
-    };
+    }
   };
 
   const deselectHexagon = () => {
@@ -268,7 +278,7 @@ const SidebarViewDetail = ({
       setDeselectButtonLabel("Deselect Hexagon");
       setShowButtonDisabled(false);
       setConfirmButtonDisabled(false);
-    };
+    }
   };
 
   const exitEdit = () => {
